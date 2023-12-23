b48ff2ac8a6c941be39f3277816e5e7a5444f29a
Sandra <sandra.sini@ecoles-epsi.net>
2023-12-23 14:33:37 +0100

    qcms back-end v2

--- controllers/qcms.js
+++ controllers/qcms.js
@@ -1,4 +1,4 @@
-const {qcms, addQcm, addQuestion} = require('../models/inmemory');
+const {qcms, questions, answers, addQcm, addQuestion} = require('../models/inmemory');
 
 const displayQcms = (req, res) => {
     res.render('qcms', {qcms: qcms});
@@ -16,33 +16,39 @@ const displayQcmDetailed = (req, res) => {
     const id = Number(req.params.qcmid);
     console.log(id);
     const qcm = qcms.find((element) => element.id === id);
-    res.render('qcm', {qcm});
+    let qcm_questions = [];
+    if (qcm) {
+        qcm_questions = questions.filter((question) => question.id_qcm === id);
+    }
+
+    let question_answers = [];
+    qcm_questions.forEach((question) => {
+        const answersForQuestion = answers.filter((answer) => answer.id_question === question.id);
+        question_answers.push({ question, answers: answersForQuestion });
+    });
+    
+    res.render('qcm', {qcm, questions: question_answers, answers: answers});
 }
 
 const createNewForm = (req, res) => {
     console.log(req.body);
-    let qcm = addQcm(
-        {
-            name: req.body.name,
-            theme: req.body.theme,
-            nb_points: req.body.nb_points,
-        }
-    );
-    let question = addQuestion(
-        {
+    let qcm = addQcm({
+        name: req.body.name,
+        theme: req.body.theme,
+        nb_points: req.body.nb_points,
+    });
+
+    // ajout des questions au QCM lié
+    req.body.questions.forEach((rawQuestion) => {
+        let question = addQuestion({
             id_qcm: qcm.id,
-            title: req.body.title,
-            points: req.body.points
-        }
-    );
-    addAnswer(
-        {
-            id_question: question.id,
-            title: req.body.title,
-            correct_answer: req.body.correct_answer
-        }
-    );
-    res.redirect('/qcms');
+            title: rawQuestion.title,
+            points: rawQuestion.points,
+            answers: rawQuestion.answers
+        });
+    });
+
+    res.redirect('/qcms/qcms');
 }
 
 module.exports = {displayQcms, displayQcmJson, displayFormQcm, createNewForm, displayQcmDetailed};
--- models/inmemory.js
+++ models/inmemory.js
@@ -41,7 +41,43 @@ const answers = [
     ),
     new Answer(
         {id: 3, id_question: 0, title: 'Le surnom d\'une célébrité', correct_answer: false}
-    )
+    ),
+    new Answer(
+        {id: 4, id_question: 1, title: 'Front-end', correct_answer: true}
+    ),
+    new Answer(
+        {id: 5, id_question: 1, title: 'Back-end', correct_answer: false}
+    ),
+    new Answer(
+        {id: 6, id_question: 1, title: 'Les deux', correct_answer: false}
+    ),
+    new Answer(
+        {id: 7, id_question: 1, title: 'Aucun des deux', correct_answer: false}
+    ),
+    new Answer(
+        {id: 8, id_question: 2, title: 'Jean-Luc Mélenchon', correct_answer: false}
+    ),
+    new Answer(
+        {id: 9, id_question: 2, title: 'Elon Musk', correct_answer: false}
+    ),
+    new Answer(
+        {id: 10, id_question: 2, title: 'Brendan Eich', correct_answer: true}
+    ),
+    new Answer(
+        {id: 11, id_question: 2, title: 'Philippe Poutou', correct_answer: false}
+    ),
+    new Answer(
+        {id: 12, id_question: 3, title: 'Aucune, Java est l\'abbréviation de Javascript', correct_answer: false}
+    ),
+    new Answer(
+        {id: 13, id_question: 3, title: 'Ce sont deux langages complètement différents', correct_answer: true}
+    ),
+    new Answer(
+        {id: 14, id_question: 3, title: 'Java est une marque d\'électroménager', correct_answer: false}
+    ),
+    new Answer(
+        {id: 15, id_question: 3, title: 'Java est le nom de l\'entreprise qui a créé Javascript', correct_answer: false}
+    ),
 ];
 
 function addQcm(rawObject) {
@@ -68,49 +104,57 @@ function addQcm(rawObject) {
 }
 
 function addQuestion(rawObject) {
-
     let maxId = 0;
 
     questions.forEach((question) => {
         if (maxId < question.id) {
-            maxId = question.id
+            maxId = question.id;
         }
-    })
+    });
+
     // création de la question
-    const question = new Question(
-        {
-            id: maxId + 1,
-            id_qcm: rawObject.id_qcm,
-            title: rawObject.title,
-            points: Number(rawObject.points)
-        }
-    );
+    const question = new Question({
+        id: maxId + 1,
+        id_qcm: rawObject.id_qcm,
+        title: rawObject.title,
+        points: Number(rawObject.points)
+    });
+
     // ajout de la question au QCM lié
     questions.push(question);
+
+    // ajout des réponses à la question liée
+    rawObject.answers.forEach((rawAnswer) => {
+        addAnswer({
+            id_question: question.id,
+            title: rawAnswer.title,
+            correct_answer: rawAnswer.correct_answer
+        });
+    });
+
     return question;
 }
 
 function addAnswer(rawObject) {
-
     let maxId = 0;
 
     answers.forEach((answer) => {
         if (maxId < answer.id) {
-            maxId = answer.id
+            maxId = answer.id;
         }
-    })
+    });
+
     // création de la réponse
-    const answer = new Answer(
-        {
-            id: maxId + 1,
-            id_question: rawObject.id_question,
-            title: rawObject.title,
-            correct_answer: rawObject.correct_answer
-        }
-    );
+    const answer = new Answer({
+        id: maxId + 1,
+        id_question: rawObject.id_question,
+        title: rawObject.title,
+        correct_answer: rawObject.correct_answer
+    });
+
     // ajout de la réponse à la question liée
     answers.push(answer);
     return answer;
 }
 
-module.exports = {qcms, questions, answers, addQcm, addQuestion, addAnswer};
+module.exports = { qcms, questions, answers, addQcm, addQuestion, addAnswer};
